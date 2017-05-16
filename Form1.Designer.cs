cecec8ea3cdba1bd7524078c4bcd89cc450e258a
liuyingliuyun <1481680882@qq.com>
2017-05-16 17:34:20 +0800

    winform  包含cskin

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.skinButton1 = new CCWin.SkinControl.SkinButton();
+            this.skinLabel1 = new CCWin.SkinControl.SkinLabel();
+            this.skinListBox1 = new CCWin.SkinControl.SkinListBox();
+            this.SuspendLayout();
+            // 
+            // skinButton1
+            // 
+            this.skinButton1.BackColor = System.Drawing.Color.Transparent;
+            this.skinButton1.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.skinButton1.DownBack = null;
+            this.skinButton1.Location = new System.Drawing.Point(400, 49);
+            this.skinButton1.MouseBack = null;
+            this.skinButton1.Name = "skinButton1";
+            this.skinButton1.NormlBack = null;
+            this.skinButton1.Size = new System.Drawing.Size(261, 155);
+            this.skinButton1.TabIndex = 0;
+            this.skinButton1.Text = "skinButton1";
+            this.skinButton1.UseVisualStyleBackColor = false;
+            // 
+            // skinLabel1
+            // 
+            this.skinLabel1.AutoSize = true;
+            this.skinLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel1.BorderColor = System.Drawing.Color.White;
+            this.skinLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel1.Location = new System.Drawing.Point(489, 243);
+            this.skinLabel1.Name = "skinLabel1";
+            this.skinLabel1.Size = new System.Drawing.Size(69, 17);
+            this.skinLabel1.TabIndex = 1;
+            this.skinLabel1.Text = "skinLabel1";
+            // 
+            // skinListBox1
+            // 
+            this.skinListBox1.Back = null;
+            this.skinListBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.skinListBox1.FormattingEnabled = true;
+            this.skinListBox1.Location = new System.Drawing.Point(400, 263);
+            this.skinListBox1.Name = "skinListBox1";
+            this.skinListBox1.Size = new System.Drawing.Size(239, 69);
+            this.skinListBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 395);
+            this.Controls.Add(this.skinListBox1);
+            this.Controls.Add(this.skinLabel1);
+            this.Controls.Add(this.skinButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private CCWin.SkinControl.SkinButton skinButton1;
+        private CCWin.SkinControl.SkinLabel skinLabel1;
+        private CCWin.SkinControl.SkinListBox skinListBox1;
+    }
+}
+
